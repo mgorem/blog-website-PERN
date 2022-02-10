9fc8f09097dfbdf230180648a54e0d965f0e15a8
mgorem <oremgitonga22@gmail.com>
2022-02-10 21:13:33 +0300

    change project structure, add components

---
+++ src/hooks/callback.js
@@ -0,0 +1,12 @@
+//User is redirected here after logging in with Autho-
+//then to authcheck page-then to homepage
+
+import React from 'react'
+
+const Callback = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Callback;
---
+++ src/hooks/privatecomponent.js
@@ -0,0 +1,11 @@
+//used in a private route and be only accessible by authenticated users
+
+import React from 'react'
+
+const PrivateComponent = props => (
+    <div>
+      Private Component
+    </div>
+);
+
+export default PrivateComponent;
